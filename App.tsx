47d63d1c23cd085a6e7a3a4b97ba012a295602a3
Julio Davila <juliodacer@gmail.com>
2023-07-31 21:49:52 -0500

    config icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <Icon name='rightcircle' size={20} color={'red'} />
     </View>
   )
 }
